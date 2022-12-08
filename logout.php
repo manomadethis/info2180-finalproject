fce3b94dd9c62adaf0ace59eca5e16bae99a2459
Clergeschist <114242359+Kareem-Pingue@users.noreply.github.com>
2022-12-08 16:26:24 -0500

    Update logout.php

--- logout.php
+++ logout.php
@@ -1,3 +1,13 @@
-<?php
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <script type= "text/javascript" src="../js/logout.js"></script>
+</head>
+</html>
+
+<?php 
 session_start();
 session_destroy();
+?>
